cf951879cdfa3305ce534ea3a0ff44a1c9845efd
sannithreddybanala <sbanala@cbu.edu>
2023-07-21 13:34:28 -0500

    exercise-set-5

--- src/ex_5_2.py
+++ src/ex_5_2.py
@@ -7,6 +7,7 @@ This module contains an entry point that
 - writes the processed data to a file called `ex_5_2-processed.csv`
 """
 import numpy as np
+import os
 
 try:
     from src.util import get_repository_root
@@ -16,11 +17,10 @@ except ImportError:
 
 if __name__ == "__main__":
 
-    # Use these predefined input / output files
     root_dir = get_repository_root()
     INFILE = root_dir / "data" / "ex_5_2-data.csv"
     OUTFILE = root_dir / "outputs" / "ex_5_2-processed.csv"
-
-    # Complete the data processing steps using numpy here.
-
-    # Save the output to OUTFILE using numpy routines.
+    input_data = np.loadtxt(INFILE)
+    processed = (input_data - input_data.mean(axis=0)) / input_data.std(axis=0)
+    os.makedirs(root_dir / "outputs", exist_ok=True)
+    np.savetxt(OUTFILE, processed, fmt='%.2e')
--- src/ex_5_3.py
+++ src/ex_5_3.py
@@ -9,11 +9,10 @@ import numpy as np
 from argparse import ArgumentParser
 
 if __name__ == "__main__":
-    # Create your argument parser object here.
-    # Collect the filename arguments from the command line
-    # Rewrite your 5_3 logic here so that it utilizes the arguments passed from the command line.
-
-    # Tests will run your command using a system call.
-    # To test your program with arguments, run it from the command line
-    # (see README.md for more details)
-    pass
+    parser = ArgumentParser(description='This program applies a standard scale transform to the data in infile and writes it to outfile.')
+    parser.add_argument('infile')
+    parser.add_argument('outfile')
+    args = parser.parse_args()
+    data = np.loadtxt(args.infile)
+    processed = (data - data.mean(axis=0)) / data.std(axis=0)
+    np.savetxt(args.outfile, processed, fmt='%.2e')
